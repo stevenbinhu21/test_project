9ccb2d7bb251ef5fb208794377cac748d47e6304
stevenbinhu21@hotmail.com <stevenbinhu21@hotmail.com>
2018-08-22 22:38:37 +0800

    commit on my-new-feature-branch, adding git_guide.txt and hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+"""
+	hello_word.py
+
+	Created by stevenbinhu
+	2018.08.22
+	email: stevenbinhu@hotmail.com
+"""
+
+#encoding=utf-8
+
+import sys
+
+if __name__ == "__main__":
+	print("Hello World!")
+
+
